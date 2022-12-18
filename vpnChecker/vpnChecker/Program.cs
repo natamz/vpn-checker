37874b7f2f422216fcaeab71a94c020cc665a9ff
nata <104881649+natamz@users.noreply.github.com>
2022-12-19 08:47:39 +0900

    add vpn checker

--- vpnChecker/vpnChecker/Program.cs
+++ vpnChecker/vpnChecker/Program.cs
@@ -1,3 +1,5 @@
+using System.Net.NetworkInformation;
+
 namespace vpnChecker
 {
     internal static class Program
@@ -8,9 +10,60 @@ namespace vpnChecker
         [STAThread]
         static void Main()
         {
-            // To customize application configuration such as set high DPI settings or default font,
-            // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
+
+            var o_path = Path.Combine(Application.StartupPath, "img", "o.ico");
+            var x_path = Path.Combine(Application.StartupPath, "img", "x.ico");
+
+            var notifyIcon = new StatusNotifyIcon(x_path);
+            Application.ApplicationExit += (_, _) => notifyIcon.Dispose();
+
+            SetToolStripMenuItems(notifyIcon);
+
+            var timer = new System.Timers.Timer(1000);
+            timer.Elapsed += (sender, e) =>
+            {
+                if (checkVpnConnection())
+                {
+                    notifyIcon.SetIcon(o_path);
+                }
+                else
+                {
+                    notifyIcon.SetIcon(x_path);
+                }
+            };
+
+            timer.Start();
+
+            Application.Run();
+        }
+
+        /// <summary>
+        /// ToolStripMenuItemを設定
+        /// </summary>
+        /// <param name="notifyIcon"></param>
+        private static void SetToolStripMenuItems(StatusNotifyIcon notifyIcon)
+        {
+            notifyIcon.AddToolStripMenuItem(new ToolStripMenuItem("終了", null, (_, _) => Application.Exit()));
+        }
+
+        /// <summary>
+        /// VPN接続判定
+        /// </summary>
+        /// <returns></returns>
+        private static bool checkVpnConnection()
+        {
+            foreach (var networkInterface in NetworkInterface.GetAllNetworkInterfaces())
+            {
+                if (networkInterface.OperationalStatus == OperationalStatus.Up &&
+                    networkInterface.NetworkInterfaceType == NetworkInterfaceType.Ppp)
+                {
+                    // vpn接続中
+                    return true;
+                }
+            }
+
+            return false;
         }
     }
 }
---
+++ vpnChecker/vpnChecker/StatusNotifyIcon.cs
@@ -0,0 +1,51 @@
+namespace vpnChecker;
+
+internal class StatusNotifyIcon : IDisposable
+{
+    private readonly NotifyIcon _notifyIcon;
+
+    public StatusNotifyIcon(string iconFilePath)
+    {
+        _notifyIcon = new NotifyIcon();
+        _notifyIcon.ContextMenuStrip = new ContextMenuStrip();
+
+        SetIcon(iconFilePath);
+
+        _notifyIcon.Visible = true;
+        _notifyIcon.Text = "";
+    }
+
+    /// <summary>
+    /// ToolStripMenuItemを追加
+    /// </summary>
+    /// <param name="item"></param>
+    public void AddToolStripMenuItem(ToolStripMenuItem item)
+    {
+        _notifyIcon.ContextMenuStrip.Items.Add(item);
+    }
+
+    /// <summary>
+    /// アイコン画像を設定
+    /// </summary>
+    /// <param name="iconFilePath"></param>
+    public void SetIcon(string iconFilePath)
+    {
+        if (!File.Exists(iconFilePath))
+        {
+            return;
+        }
+
+        _notifyIcon.Icon = new Icon(iconFilePath);
+    }
+
+    /// <summary>
+    /// クリック時のイベント
+    /// </summary>
+    public event EventHandler Click
+    {
+        add { _notifyIcon.Click += value; }
+        remove { _notifyIcon.Click -= value; }
+    }
+
+    public void Dispose() => _notifyIcon.Dispose();
+}
